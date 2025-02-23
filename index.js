0c8eaf8f0395c489a159e163e77169a54c6571e3
Gad-Iradufasha <legmeekmc4@gmail.com>
2025-02-23 23:39:13 +0200

    Generating and validating JWT

--- index.js
+++ index.js
@@ -14,3 +14,41 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`App is up and running on http://localhost:${PORT}`);
 });
+
+// 01. Generating JWT
+app.post("/user/generate-token", (req, res) => {
+    // A. Validate user here
+    // B. Then Generate JWT Token
+    let jwtSectetKey = process.env.JWT_SECRET_KEY
+    let data = {
+        time: Date(),
+        userId : 30
+    }
+
+    const token  = jwt.sign(data, jwtSectetKey);
+    res.send(token)
+})
+
+// 02. Verifying/Validating JWT
+app.get("/user/verify-token", (req, res) => {
+    // Tokens are generally passed in header of request
+    // Due to security reasons.
+    let tokenHeaderKey = process.env.JWT_HEADER_KEY;
+    let jwtSectetKey = process.env.JWT_SECRET_KEY;
+
+    try {
+        const token = req.header(tokenHeaderKey);
+        const verified = jwt.verify(token, jwtSectetKey);
+        if(verified) {
+            // Access Granted
+            return res.send("Valid Token: Successfully Verified");
+        } else {
+            // Access Denied
+            // return res.status(401).send("Invalid Token");
+            return res.send(401).send(error);
+        }
+    } catch (error) {
+        // Access Denied
+        return res.status(401).send("Invalid Token");
+    }
+});
