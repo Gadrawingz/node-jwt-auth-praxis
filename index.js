d100a5e271e1fcdfd8ac86af7e90bf359c00e3ad
Gad-Iradufasha <legmeekmc4@gmail.com>
2025-02-23 22:52:47 +0200

    First commit on app

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+const app = express();
+
+// Set up global configuration access
+dotenv.config();
+let PORT = process.env.PORT || 4000;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`App is up and running on http://localhost:${PORT}`);
+});
